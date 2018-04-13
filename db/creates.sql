ad5ebd28328caaf50f1eaccc81baf41e8ff83d9a
Isaac Batista <daniel.bautista@pengostores.com>
2018-04-13 10:38:13 -0500

    Refactor

--- db/creates.sql
+++ db/creates.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE tests;
 
-use tests; 
+USE tests; 
 
 CREATE TABLE users (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -8,4 +8,6 @@ CREATE TABLE users (
     lastname VARCHAR(30) NOT NULL,
     email VARCHAR(50),
     created_at TIMESTAMP
-)
+);
+
+INSERT INTO users (name, lastname, email) VALUES ('Isaac', 'Batista', 'isaac@pengo.com'), ('Mar', 'Perez', 'mar@pengo.com'), ('Daniel', 'Batista', 'daniel@pengo.com'); 
--- src/index.php
+++ src/index.php
@@ -1,1 +1,32 @@
-<?php echo 'Hello'; ?>
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+echo 'Hola<pre>';
+
+define('USERNAME', "root");
+define('PASSWORD', "mysql");
+define('HOST', "127.0.0.1:3307");
+define('DB', "magento2");
+
+var_dump($_SERVER);
+echo 'variable host: ' . getenv('DB_HOST'); exit(0);
+/**
+ * @return \PDO
+ */
+function getPDO()
+{
+    $username = USERNAME;
+    $password = PASSWORD;
+    $host = HOST;
+    $db = DB;
+        
+    return new \PDO("mysql:host={$host};dbname={$db}", $username, $password);
+}
+
+try {
+    $pdo = getPDO();
+
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
